a7e133d0c4505cffd9047e636244eeb4c6924cd8
DHAIRYAKARSHINI <2200031455@kluniversity.in>
2026-09-13 13:54:46 +0530

    Added Vercel API configuration

--- api/index.py
+++ api/index.py
@@ -1,4 +1,18 @@
-def handler(request):
-    return {
-        "message": "AI Travel Assistant API is working!"
-    }
+from http.server import BaseHTTPRequestHandler
+import json
+
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        response = {
+            "message": "AI Travel Assistant API is working!"
+        }
+
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+
+        self.wfile.write(
+            json.dumps(response).encode("utf-8")
+        )
